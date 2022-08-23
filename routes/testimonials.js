b1db3ba0d04c16505fd19814da3377b50fa52789
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2022-08-23 19:28:21 -0300

    added testimonial update route

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { testimonialFields } = require('../middlewares/bodyValidation');
-const { postTestimonial } = require('../controllers/testimonialsController');
+const { postTestimonial, putTestimonial } = require('../controllers/testimonialsController');
 
 // create a new testimonial
 router.post('/', testimonialFields, postTestimonial);
+
+router.put('/:id', putTestimonial)
 
 module.exports = router;
