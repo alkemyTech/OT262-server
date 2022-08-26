b2b37021c002ace0521728f82ca70a6351fcd714
Javier Sepic <javier.sepic@gmail.com>
2022-08-26 13:24:33 -0300

    get all activities endpoint added

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -55,4 +55,16 @@ const updateActivity = async (req, res) => {
     }
 }
 
-module.exports = {postActivity, updateActivity};
+const getActivities = async (req, res) => {
+try {
+    const activities = await Activity.findAll();
+    res.status(200).json({ activities })
+} catch (error) {
+    res.status(500).json({
+        error: error.message
+    });
+}
+
+}
+
+module.exports = {postActivity, updateActivity, getActivities};
