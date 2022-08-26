671ff5e6e871cd615c30eba5f7219f05becc913e
FrancoSpinelli <81250647+FrancoSpinelli@users.noreply.github.com>
2022-08-26 10:24:52 -0300

    exports functions

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { list, create, update, destroy } = require('../controllers/categoriesController');
+const { getCategories, createCategory, updateCategory, deleteCategory } = require('../controllers/categoriesController');
 const validation = require('../middlewares/categoryValidation');
 
-router.get('/', list);
-router.post('/', validation, create);
-router.put('/:id', update);
-router.delete('/:id', destroy);
+router.get('/', getCategories);
+router.post('/', validation, createCategory);
+router.put('/:id', updateCategory);
+router.delete('/:id', deleteCategory);
 
 module.exports = router;
