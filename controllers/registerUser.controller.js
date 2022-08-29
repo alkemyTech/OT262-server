797e0b2b46d601a15ce9f915cf1c672629b34054
marian.eq <mariano.eq@gmail.com>
2022-08-29 22:07:25 +0100

    logic to register users added

--- controllers/registerUser.controller.js
+++ controllers/registerUser.controller.js
@@ -1,37 +1,41 @@
-const User = require('../models/user')
+const { validationResult } = require("express-validator");
+const { User } = require("../models");
 const bcrypt = require("bcryptjs");
 
-const registerUser =  async (req,res) => {
-
-    try{
-        const { firstName, lastName, email, password, image } = req.body;
-
-        if (!(email && password && firstName && lastName)) {
-            res.status(400).send("All input is required");
-          }
-
-          const oldUser = await User.findOne({ where: { email: email}})
-          
-          if(oldUser){
-            res.status(200).send("User already exists")
-          }
-
-           const encryptedPassword = await bcrypt(password,10);
-
-            const newUser = await User.create({ 
-                firstName,
-                lastName,
-                email,
-                password:encryptedPassword,
-                image
-            });
-
-            res.status(200).send("User created successfully", newUser)
-    }
-    catch(err) {
-        res.status(500).send("Error creating user", err)
+const registerUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors });
+  }
+  const { firstName, lastName, email, password, image } = await req.body;
+  try {
+    if (!(email && password && firstName && lastName)) {
+      res.status(400).send("All input is required");
     }
 
-}
+    const oldUser = await User.findOne({ where: { email: email } });
 
-module.exports = {registerUser}
+    if (oldUser) {
+      res.status(400).send("User already exists");
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: encryptedPassword,
+      image,
+    });
+
+    res
+      .status(200)
+      .json({ msg: "User created successfully", payload: newUser });
+  } catch (err) {
+    res.status(400).json({ msg: "Error creating user", err });
+    console.log(err)
+  }
+};
+
+module.exports = { registerUser };
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const {registerValidation} = require('../middlewares/registervalidation')
-const {registerUser} = require('../controllers/registerUser.controller')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/auth/register', registerValidation, registerUser);
+
 
 
 
