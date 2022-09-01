c31a957964c833c499a668053ed82a4be76bb071
Gonzalo <espindolamgonzalo@gmail.com>
2022-09-01 19:16:46 -0300

    added edit slides controller

--- controllers/slidesController.js
+++ controllers/slidesController.js
@@ -1,4 +1,4 @@
-const { findAllSlides } = require('../services/slidesService');
+const { findAllSlides, updateSlide, findSlide } = require('../services/slidesService');
 
 const getSlides = async (req, res) => {
     try {
@@ -14,4 +14,32 @@ const getSlides = async (req, res) => {
     }
 }
 
-module.exports = { getSlides };
+const putSlides = async (req, res) => {
+
+    const { id } = req.params;
+
+    const foundSlide = await findSlide(id);
+
+    if(foundSlide === null) {
+        return res.status(404).json({
+            error: `Slide with id ${id} not found` 
+        })
+    }
+
+    const { imageUrl, text, order, organizationId } = req.body;
+
+    try {
+        const updatedSlide = await updateSlide(foundSlide, { imageUrl, text, order, organizationId });
+        res.status(200).json({
+            msg: "Slide updated succesfully",
+            updatedSlide
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "Something has gone wrong",
+            errors: error.message
+        })
+    }
+}
+
+module.exports = { getSlides, putSlides };
