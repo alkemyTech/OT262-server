082747ed8c51980f6d32df7b33e745ae96c6236c
marian.eq <mariano.eq@gmail.com>
2022-09-01 23:04:03 +0100

    Create token function done

--- controllers/jwtAuth.controller.js
+++ controllers/jwtAuth.controller.js
@@ -3,7 +3,7 @@ var jwt = require("jsonwebtoken");
 
 //This should be imported into userRegister. Right below user.create function.
 const createToken = (id,firstName,email) => {
-  return jwt.sign({ id,firstName,email}, process.env.TOKEN_KEY, { expiresIn: "3h" });
+  return jwt.sign({ id,firstName,email}, process.env.SECRET_KEY, { expiresIn: 360000 });
 };
 
 module.exports = {createToken}
