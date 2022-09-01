167df860c5a67684197132e4a46bcd9d391aee3c
Gonzalo <espindolamgonzalo@gmail.com>
2022-09-01 19:17:02 -0300

    added edit slides services

--- services/slidesService.js
+++ services/slidesService.js
@@ -5,4 +5,14 @@ const findAllSlides = async () => {
     return allSlides;
 }
 
-module.exports = { findAllSlides };
+const findSlide = async (id) => {
+    const foundSlide = await Slides.findOne({ where: { id:id } });
+    return foundSlide;
+}
+
+const updateSlide = async (toUpdateSlide, newData) => {
+    const updatedSlide = await toUpdateSlide.update(newData);
+    return updatedSlide;
+}
+
+module.exports = { findAllSlides, updateSlide, findSlide };
