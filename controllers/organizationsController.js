93bff0559857263b5bc3ac74efc85c067cecace5
FrancoSpinelli <81250647+FrancoSpinelli@users.noreply.github.com>
2022-09-01 17:18:53 -0300

    update organization

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -15,6 +15,44 @@ const getOrganizations = async (req, res) => {
 	}
 };
 
+const updateOrganization = async (req, res) => {
+	const { name, image, phone, welcomeText, address } = req.body;
+	const { id } = req.params;
+
+	try {
+		const organizationInDB = await Organization.findByPk(id);
+
+		if (!organizationInDB) {
+			return res.status(400).json({
+				status: "error",
+				message: "Organization is not found in DB",
+			});
+		}
+		const updateOrganization = {
+			name: name || organizationInDB.name,
+			image: image || organizationInDB.image,
+			phone: phone || organizationInDB.phone,
+			welcomeText: welcomeText || organizationInDB.welcomeText,
+			address: address || organizationInDB.address,
+		};
+		await Organization.update(updateOrganization, {
+			where: {
+				id,
+			},
+		});
+		return res.status(200).json({
+			status: "successful",
+			data: updateOrganization,
+		});
+	} catch (error) {
+		return res.status(400).json({
+			status: "error",
+			message: error,
+		});
+	}
+};
+
 module.exports = {
 	getOrganizations,
+	updateOrganization
 };
--- routes/organizations.js
+++ routes/organizations.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getOrganizations } = require("../controllers/organizationsController");
+const { getOrganizations, updateOrganization } = require("../controllers/organizationsController");
 
 router.get('/', getOrganizations)
+router.put('/:id', updateOrganization)
 
 module.exports = router;
