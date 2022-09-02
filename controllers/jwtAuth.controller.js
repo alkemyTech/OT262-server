0d0b44d954aaf7981528a832b8b89991ce8da7d5
marian.eq <mariano.eq@gmail.com>
2022-09-02 18:54:41 +0100

    secret_key added and conflicts sorted

--- controllers/jwtAuth.controller.js
+++ controllers/jwtAuth.controller.js
@@ -2,8 +2,8 @@ var jwt = require("jsonwebtoken");
 
 
 //This should be imported into userRegister. Right below user.create function.
-const createToken = (id,firstName,email) => {
-  return jwt.sign({ id,firstName,email}, process.env.SECRET_KEY, { expiresIn: 360000 });
+const createToken = (user) => {
+  return jwt.sign({user}, process.env.SECRET_KEY, { expiresIn: 360000 });
 };
 
 module.exports = {createToken}
