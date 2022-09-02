0d40a891f8082166eaa01f04778ebe04a7c93539
FrancoSpinelli <81250647+FrancoSpinelli@users.noreply.github.com>
2022-09-02 18:52:13 -0300

    add middleware checkadmin

--- routes/organization.js
+++ routes/organization.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getOrganization, updateOrganization } = require('../controllers/organizationController');
+const { checkAdmin } = require('../middlewares/checkAdmin');
 
 router.get('/', getOrganization);
-router.put('/', updateOrganization);
+router.put('/', checkAdmin, updateOrganization);
 
 module.exports = router;
