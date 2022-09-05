bec483d12370b119a2e87059775399650d481526
marian.eq <mariano.eq@gmail.com>
2022-09-05 23:03:41 +0100

    functionality to list all contacts done

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,54 +1,40 @@
 const { validationResult } = require("express-validator");
-const {createContact } = require("../services/contactsServices");
+const { createContact } = require("../services/contactsServices");
 
 const postContacts = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
-    return res.status(400).json({ errors:errors });
-
+    return res.status(400).json({ errors: errors });
   }
 
-  const {name,phone,email,message} = req.body
+  const { name, phone, email, message } = req.body;
 
-  try{
-    const newContact = await createContact({name,phone,email,message})
-    res.status(200).json({ 
-        message:"Contact created",
-        payload:newContact
-    })
-}
-    catch(err){
-        res.status(500).json({ message: "There's been an error"})
-    }
-}
+  try {
+    const newContact = await createContact({ name, phone, email, message });
+    res.status(200).json({
+      message: "Contact created",
+      payload: newContact,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "There's been an error" });
+  }
+};
 
 //Get all contacts
-const getAllContacts = async (req,res) => {
+const getAllContacts = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
-    return res.status(400).json({ errors:errors });
-
+    return res.status(400).json({ errors: errors });
   }
-  try{
-  if(req.user.roleID === 2){
-     const getContacts = await Contacts.findAll()
-      res.status(200).json({ 
-          message:"Contacts listed successfully",
-          payload:getContacts 
-      })
-  }else{
-    res.status(201).json({message:"You have no access!"})
+  try {
+    const getContacts = await Contacts.findAll();
+    res.status(200).json({
+      message: "Contacts listed successfully",
+      payload: getContacts,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "There's been an error" });
   }
+};
 
-}
-
-  catch (error) {
-    res.status(500).json({ message: "There's been an error"})
-  }
-
-}
-
-
-module.exports = {getAllContacts, postContacts}
+module.exports = { getAllContacts, postContacts };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {contactField, validateToken} = require("../middlewares/contactValidation")
+const {contactField, validateToken, checkAdmin} = require("../middlewares")
+
 const {postContacts, getAllContacts} = require("../controllers/contactsController")
 
 
 //Create new contact
 router.post("/", contactField ,postContacts)
 
-
 //Get all contacts
-router.get("/", validateToken, getAllContacts)
+router.get("/", validateToken, checkAdmin, getAllContacts)
 
 module.exports = router;
