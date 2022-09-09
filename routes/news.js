4f5c205b45d0f5327918b55d522127b23f24f879
Maximiliano Sinopoli <m_sinopoli@hotmmail.com>
2022-09-09 10:18:34 +0100

    Get news details controllers created

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllNews, createNews, deleteNewsById } = require("../controllers/newsController");
+const { getAllNews, createNews, deleteNewsById, getNewsById } = require("../controllers/newsController");
 const { newsFieldCheck } = require("../middlewares/newsValidation");
 
 router.get('/', getAllNews)
 router.post('/', newsFieldCheck, createNews)
 router.delete('/:id', deleteNewsById)
+router.get('/:id', getNewsById)
 
 module.exports = router;
