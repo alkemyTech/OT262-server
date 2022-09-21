596e71a8dfe7eaf7c4cc746b3d201f0d0f45d79f
Javier Sepic <javier.sepic@gmail.com>
2022-09-21 17:35:27 -0300

    minor fix in news validation

--- middlewares/newsValidation.js
+++ middlewares/newsValidation.js
@@ -2,8 +2,7 @@ const { check } = require('express-validator');
 
 const newsFieldCheck = [
     check('name', 'Name is required').not().isEmpty(),
-    check('image', 'Image is required').not().isEmpty(),
-    check('type', 'Type is required').not().isEmpty()
+    check('image', 'Image is required').not().isEmpty()
 ];
 
 module.exports = { newsFieldCheck };
