29505beb371f9bb573d9aa264ac9ac06db8cd1df
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2022-09-24 18:43:12 -0300

    fix errors

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { getAllUsers } = require('../controllers/userController')
-const { deleteUser } = require('../controllers/userController')
+const { getAllUsers, deleteUser, putUser } = require('../controllers/userController')
 const { validateToken } = require('../middlewares/validateToken');
 const { checkAdmin } = require('../middlewares/checkAdmin');
 
 router.get('/', validateToken, checkAdmin, getAllUsers);
 router.delete('/:id', validateToken, checkAdmin, deleteUser);
-
-
+router.put('/:id', validateToken, checkAdmin, putUser)
 
 module.exports = router;
